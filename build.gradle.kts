088b3eeb7eed2688578a5141f4cb54a0e621496b
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-12-01 23:02:26 +0900

    feat: add project

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.21"
-    id("org.jetbrains.compose") version "1.0.0-alpha3"
+    kotlin("jvm") version "1.5.31"
+    id("org.jetbrains.compose") version "1.0.0-beta5"
 }
 
-group = "jp.kaleidot725"
+group = "me.katz"
 version = "1.0"
 
 repositories {
